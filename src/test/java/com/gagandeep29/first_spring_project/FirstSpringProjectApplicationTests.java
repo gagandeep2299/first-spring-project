8766447d0ad797d507454b9cf735f5f16644b872
gagands1 <gagands1@amdocs.com>
2026-04-26 18:55:45 +0530

    first commit

---
+++ src/test/java/com/gagandeep29/first_spring_project/FirstSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gagandeep29.first_spring_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
